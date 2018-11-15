2fcdfdce66e25db6c3134d6a0091c2e313a23a23
arman sareskandi <42113160+MrArmaN2003@users.noreply.github.com>
2018-11-16 02:39:30 +0330

    Add files via upload
    Euler projects done with python

---
+++ Euler Second Project.py
@@ -0,0 +1,22 @@
+def IsEven(n):
+    if n % 2 == 0:
+        return True
+    else:
+        return False
+    
+first = 1
+seccond = 2
+
+sum = 0
+
+while (first < 4000000):
+    #print ('my first number is', first)
+    if IsEven(first):
+       #print ('oh it is even')
+        sum = sum + first
+        #print ('---> now the sum is', sum)
+    new = first + seccond
+    first = seccond
+    seccond = new 
+    
+print (sum)
